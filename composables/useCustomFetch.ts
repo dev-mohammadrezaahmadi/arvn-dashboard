9100aec40fc3353110d90fdc026da6f2ff87275d
dev-mohammadrezaahmadi <dev.mohammadrezaahmadi@outlook.com>
2023-12-28 19:04:00 +0330

    feat: set auth token on every request on api interceptor

--- composables/useCustomFetch.ts
+++ composables/useCustomFetch.ts
@@ -1,8 +1,22 @@
 import { $fetch, type FetchOptions } from "ofetch";
+import Keys from "~/constants/Keys";
 
 export function useCustomFetch(url: string, options: FetchOptions = {}) {
+  const token = useCookie(Keys.AUTH_TOKEN);
+
   return $fetch(url, {
     ...options,
     baseURL: "https://api.realworld.io",
+    onRequest({ options }) {
+      if (token.value) {
+        options.headers = {
+          ...options.headers,
+          authorization: `Bearer ${token.value}`,
+        };
+      }
+    },
+    onResponseError({ response }) {
+      return Promise.reject(response._data);
+    },
   });
 }
