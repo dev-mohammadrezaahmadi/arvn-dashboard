33def90358e2c9ae99a41a72efcee22f9ab00621
dev-mohammadrezaahmadi <dev.mohammadrezaahmadi@outlook.com>
2023-12-29 17:36:58 +0330

    feat: style improvements

--- pages/articles/edit/[slug].vue
+++ pages/articles/edit/[slug].vue
@@ -1,33 +1,31 @@
 <template>
-  <div class="flex p-10">
-    <UForm class="w-1/2" :state="state" @submit="onFormSubmit">
+  <h1 class="px-5 py-5 text-5xl capitalize text-start font-bold">edit article</h1>
+
+  <div class="px-5 py-5 flex flex-col sm:flex-row gap-6">
+    <UForm class="flex w-full sm:w-2/3 flex-col gap-6 order-2 sm:order-1" :state="state" @submit="onFormSubmit">
       <UFormGroup label="Title" name="title">
-        <UInput v-model="state.title" />
+        <UInput size="xl" placeholder="Title" v-model="state.title" />
       </UFormGroup>
 
       <UFormGroup label="Description" name="description">
-        <UInput v-model="state.description" />
+        <UInput size="xl" placeholder="Description" v-model="state.description" />
       </UFormGroup>
 
       <UFormGroup label="Body" name="body">
-        <UTextarea v-model="state.body" />
+        <UTextarea resize :rows="10" v-model="state.body" />
       </UFormGroup>
 
-      <UButton type="submit">Submit</UButton>
+      <UButton :loading="updateArticleStatus === 'pending'" class="w-full sm:w-fit justify-center" size="xl" color="blue" type="submit" label="Submit" />
     </UForm>
 
-    <UForm :state="state" @submit="onTagFormSubmit">
+    <UForm class="w-full sm:w-1/3 order-1 sm:order-2" :state="state" @submit="onTagFormSubmit">
       <!-- tag list -->
       <UFormGroup label="Tags" name="newTag">
-        <UInput v-model="tagsFormState.newTag" placeholder="New tag" />
+        <UInput size="xl" v-model="tagsFormState.newTag" placeholder="New tag" />
       </UFormGroup>
 
-      <div class="flex flex-col gap-2">
-        <UCheckbox
-          v-for="tag in state.tagList"
-          v-model="tag.isActive"
-          :label="tag.name"
-        />
+      <div class="flex flex-col gap-2 mt-6 border border-gray-300 rounded p-4">
+        <UCheckbox v-for="tag in sortedTags" v-model="tag.isActive" :label="tag.name" />
       </div>
     </UForm>
   </div>
@@ -42,7 +40,6 @@ interface Tag {
 
 const route = useRoute();
 const router = useRouter();
-
 const toast = useToast();
 
 const aritcleId = computed(() => route.params.slug as string);
@@ -67,10 +64,11 @@ const state = ref<{
   tagList: [],
 });
 
+// set initial values
 if (fetchedArticleData.value) {
-    state.value.title = fetchedArticleData.value.article.title
-    state.value.description = fetchedArticleData.value.article.description
-    state.value.body = fetchedArticleData.value.article.body
+  state.value.title = fetchedArticleData.value.article.title
+  state.value.description = fetchedArticleData.value.article.description
+  state.value.body = fetchedArticleData.value.article.body
 }
 
 const tagsFormState = ref<{
@@ -111,7 +109,7 @@ const sortedTags = computed(() => {
   return state.value.tagList.sort((a, b) => a.name.localeCompare(b.name));
 });
 
-const { status, error, execute } = useAsyncData(
+const { status: updateArticleStatus, error, execute } = useAsyncData(
   () =>
     updateArticle({
       slug: aritcleId.value,
@@ -125,8 +123,21 @@ const { status, error, execute } = useAsyncData(
 
 async function onFormSubmit() {
   await execute();
-  router.push("/");
-  toast.add({ title: "article has been updated successfuly" });
+  if (updateArticleStatus.value === "pending") {
+  } else if (updateArticleStatus.value === "error") {
+    toast.add({
+      title: error.value?.cause as string,
+      color: "red",
+      icon: "i-heroicons-x-circle-solid",
+    });
+  } else if (updateArticleStatus.value === "success") {
+    toast.add({
+      title: "Article updated successfuly",
+      color: "green",
+      icon: "i-heroicons-check-solid",
+    });
+    router.push("/");
+  }
 }
 </script>
 
