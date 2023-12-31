458d41838e89ab60b605db812c70158ae4d73ef0
dev-mohammadrezaahmadi <dev.mohammadrezaahmadi@outlook.com>
2024-01-01 00:52:37 +0330

    fix: icons on mobile view not loaded

--- components/MobileViewNavbar.vue
+++ components/MobileViewNavbar.vue
@@ -3,17 +3,27 @@ import { SIDEBAR_LINKS } from "~/constants/navbarLinks";
 </script>
 
 <template>
-    <nav class="w-full flex justify-evenly bg-blue-600">
-        <NuxtLink v-for="item in SIDEBAR_LINKS" v-slot="{ isActive }" :to="item.route" :key="item.label">
-            <div class="p-3">
-                <UIcon v-if="isActive" class="text-white text-2xl" :name="item.activeIcon" />
-                <UIcon v-else  class="text-white text-2xl" :name="item.inactiveIcon" />
-            </div>
-            <!-- <p class="text-white">
-        {{ item.label.split(/\s+/)[0] }}
-      </p> -->
-        </NuxtLink>
-    </nav>
+  <nav class="w-full flex justify-evenly bg-blue-600">
+    <NuxtLink
+      v-for="item in SIDEBAR_LINKS"
+      v-slot="{ isActive }"
+      :to="item.route"
+      :key="item.label"
+    >
+      <div class="p-3 text-center">
+        <UIcon
+          v-if="isActive"
+          class="text-white text-2xl"
+          :name="item.activeIcon"
+          dynamic
+        />
+        <UIcon v-else class="text-white text-2xl" :name="item.inactiveIcon" dynamic />
+        <p class="text-white mt-2" :class="[isActive && 'font-bold']">
+          {{ item.label}}
+        </p>
+      </div>
+    </NuxtLink>
+  </nav>
 </template>
 
 <style scoped></style>
